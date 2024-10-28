0230aeac90448ec9bf80b9eddf49e89300ae125b
Jian Peng <pengjianaixue@hotmail.com>
2024-10-28 11:22:00 +0800

    the cmd support json config file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use clap::error::ErrorKind;
 use clap::Parser;
 use crossbeam_channel::unbounded;
-use crossterm::event::{self, Event, KeyCode};
-use serde::Serialize;
-use serde_json::Result;
+use once_cell::sync::Lazy;
+use serde::Deserialize;
+use serde_json::from_str;
 use std::collections::HashMap;
-use std::io::{BufRead, BufReader, Error, Stdout, Write};
+use std::io::Read;
 use std::os::windows::process::CommandExt;
 use std::path::Path;
 use std::process::{Child, Command, Stdio};
 use std::ptr::null_mut;
-use std::thread::{sleep, spawn};
-use std::time::Duration;
+use std::str::FromStr;
+use std::thread::spawn;
 use std::u32::MAX;
-use std::{env, io, result};
-use winapi::um::consoleapi::SetConsoleCtrlHandler;
+use std::{env, io};
 use winapi::um::winbase::{CREATE_NEW_CONSOLE, DETACHED_PROCESS};
-use winapi::um::wincon::CTRL_CLOSE_EVENT;
 use winapi::{
-    shared::minwindef::{BOOL, DWORD, FALSE, TRUE, UINT},
+    shared::minwindef::UINT,
     um::{errhandlingapi::*, winuser::*},
 };
 use windows::Win32::Foundation::{HINSTANCE, LPARAM, LRESULT, TRUE as Foundation_True, WPARAM};
@@ -39,25 +36,40 @@ struct Args {
     count: u8,
 }
 
-#[derive(Serialize)]
-struct RunContext<'a> {
+#[derive(Deserialize, Debug)]
+struct Cmd_Context<'a> {
     quick_cmd_name: &'a str,
+    run_type: Option<&'a str>,
     program_name: &'a str,
+    process_create_attr: &'a str,
     work_dir: Option<&'a str>,
-    create_falg: Option<u32>,
-    run_type: u32,
+    shortcut_key_name: &'a str,
+    #[serde(skip)]
+    shortcut_key_code: Option<u32>,
     args: Vec<&'a str>,
-    shortcut_key: u32,
 }
 
-fn run_command(run_ctxt: &RunContext) -> io::Result<Child> {
+fn get_process_attr_from_name(process_create_flag: &str) -> Option<u32> {
+    match process_create_flag {
+        "CREATE_NEW_CONSOLE" => Some(DETACHED_PROCESS),
+        "DETACHED_PROCESS" => Some(DETACHED_PROCESS),
+        _ => None,
+    }
+}
+fn run_command(run_ctxt: &Cmd_Context) -> io::Result<Child> {
     println!("[runner]-> start run <{}>\n", run_ctxt.quick_cmd_name);
     let mut cmd_runner = Command::new(run_ctxt.program_name);
     if let Some(work_dir) = run_ctxt.work_dir {
         cmd_runner.current_dir(work_dir);
     }
-    if let Some(create_falg) = run_ctxt.create_falg {
-        cmd_runner.creation_flags(create_falg);
+
+    if let Some(create_flag) = get_process_attr_from_name(run_ctxt.process_create_attr) {
+        cmd_runner.creation_flags(create_flag);
+    } else {
+        println!(
+            "[runner]-> run cmd:<{}> failed, unknow create flag:{}",
+            run_ctxt.quick_cmd_name, run_ctxt.process_create_attr
+        );
     }
 
     match cmd_runner
@@ -77,6 +89,39 @@ fn run_command(run_ctxt: &RunContext) -> io::Result<Child> {
             Err(err)
         }
     }
+}
+
+fn create_virtual_key_map() -> HashMap<&'static str, u32> {
+    let mut key_map = HashMap::new();
+
+    // key_map.insert("VK_LBUTTON", 0x01);
+    // key_map.insert("VK_RBUTTON", 0x02);
+    // key_map.insert("VK_CANCEL", 0x03);
+    // key_map.insert("VK_MBUTTON", 0x04);
+    // key_map.insert("VK_XBUTTON1", 0x05);
+    // key_map.insert("VK_XBUTTON2", 0x06);
+    // key_map.insert("VK_BACK", 0x08);
+    // key_map.insert("VK_TAB", 0x09);
+    // key_map.insert("VK_RETURN", 0x0D);
+    // key_map.insert("VK_SHIFT", 0x10);
+    // key_map.insert("VK_CONTROL", 0x11);
+    // key_map.insert("VK_MENU", 0x12);
+    // key_map.insert("VK_PAUSE", 0x13);
+    // key_map.insert("VK_CAPITAL", 0x14);
+    // key_map.insert("VK_ESCAPE", 0x1B);
+    // key_map.insert("VK_SPACE", 0x20);
+    key_map.insert("VK_NUMPAD0", 0x60);
+    key_map.insert("VK_NUMPAD1", 0x61);
+    key_map.insert("VK_NUMPAD2", 0x62);
+    key_map.insert("VK_F1", 0x70);
+    key_map.insert("VK_F2", 0x71);
+    key_map.insert("VK_F3", 0x72);
+    key_map
+}
+
+fn get_virtual_key_code(key_name: &str) -> Option<u32> {
+    let key_map = create_virtual_key_map();
+    key_map.get(key_name).copied()
 }
 
 /* system hook call back function protype */
@@ -99,12 +144,18 @@ fn run_command(run_ctxt: &RunContext) -> io::Result<Child> {
 
 /* hotkey magic number */
 const HOTKEY_ID: i32 = 0xC025DE_i32;
+static mut CONTENTS: std::string::String = String::new();
 
 // macro_rules! print_var {
 //     ($var:ident) => {
 //         println!("{} = {:?}", stringify!($var), $var);
 //     };
 // }
+static mut CMD_CONFIG_MAP: Lazy<HashMap<u32, Cmd_Context<'static>>> = Lazy::new(|| {
+    let m = HashMap::new();
+    m
+});
+
 fn main() -> io::Result<()> {
     let args = Args::parse();
     for _ in 0..args.count {
@@ -114,13 +165,39 @@ fn main() -> io::Result<()> {
     if !json_path.exists() {
         println!("File {} not exists", args.json_path)
     }
+    // let mut cmd_config_map: HashMap<u32, Cmd_Context> = HashMap::new();
+    let mut file = std::fs::File::open(json_path).unwrap();
 
+    file.read_to_string(unsafe { &mut CONTENTS })?;
+
+    let cmd_config_items: Vec<Cmd_Context> = from_str(unsafe { CONTENTS.as_str() })?;
+    for mut cmd_item in cmd_config_items {
+        println!("config_cmd : {:?}", cmd_item);
+        cmd_item.shortcut_key_code = get_virtual_key_code(cmd_item.shortcut_key_name);
+        if let Some(shortcut_key) = cmd_item.shortcut_key_code {
+            if unsafe { CMD_CONFIG_MAP.contains_key(&shortcut_key) } {
+                println!(
+                    "the cmd: [{}] shortcut key [{}] conflict",
+                    cmd_item.quick_cmd_name, cmd_item.shortcut_key_name
+                );
+            } else {
+                unsafe { CMD_CONFIG_MAP.insert(shortcut_key, cmd_item) };
+            }
+        } else {
+            println!(
+                "[Err] => cmd:[{}] shortcut key[{}] do not support",
+                cmd_item.quick_cmd_name, cmd_item.shortcut_key_name
+            );
+        }
+    }
     let (terminal_tx, terminal_rx) = unbounded();
     let (hotkey_tx, hotkey_rx) = unbounded();
     /* add the ctrl-c input handler */
     ctrlc::set_handler(move || {
         println!("received ctrl + C");
-        terminal_tx.send("exit").expect("send failed");
+        terminal_tx
+            .send(String::from_str("exit").unwrap())
+            .expect("send failed");
     })
     .expect("set handle error");
 
@@ -131,6 +208,19 @@ fn main() -> io::Result<()> {
     //     }
     // }
 
+    let hotkey_handler = hotkey_handler(unsafe { &CMD_CONFIG_MAP }, hotkey_rx);
+    let get_message_thread =
+        hotkey_register_and_monitor(unsafe { &CMD_CONFIG_MAP }, terminal_rx, hotkey_tx);
+    /* waiting for the hook process thread */
+    while !get_message_thread.is_finished() {}
+    while !hotkey_handler.is_finished() {}
+    Ok(())
+}
+
+fn hotkey_handler(
+    cmd_config_map: &'static HashMap<u32, Cmd_Context>,
+    hotkey_rx: crossbeam_channel::Receiver<i32>,
+) -> std::thread::JoinHandle<()> {
     let hotkey_handler = spawn(move || {
         loop {
             let key_msg = hotkey_rx.try_recv();
@@ -147,43 +237,39 @@ fn main() -> io::Result<()> {
                         println!("hot key process exit !");
                         break;
                     }
-                    match key_code {
-                        VK_F1 => {
-                            let run_ctxt = RunContext{quick_cmd_name: "GRAPE GDB",program_name:"python.exe",work_dir:Some("D:/Code/firmware/memblaze/gdb/grape/gdb_launcher"),create_falg:Some(DETACHED_PROCESS),run_type:0,shortcut_key:VK_F1 as u32,args: vec!["D:/Code/firmware/memblaze/gdb/grape/gdb_launcher/gdb_launcher_gui_grape.py"]};
-                            let _ = run_command(&run_ctxt);
-                        }
-                        VK_F2 => {
-                            println!("start scp image to local \n");
-                            let mut run_ctxt = RunContext{quick_cmd_name: "scp image to local",program_name:"scp.exe",work_dir:None,create_falg:Some(DETACHED_PROCESS),run_type:0,shortcut_key:VK_F2 as u32,args: vec!["pengjian@172.30.20.3:/home/pengjian/code/firmware/build/swap/images/*.axf","C:/Users/jian.peng/Desktop/Images/images"]
-                            };
-                            let _ = run_command(&run_ctxt);
-                            run_ctxt.args = vec!["pengjian@172.30.20.3:/home/pengjian/code/firmware/build/swap/images/image_allbinary.bin","C:/Users/jian.peng/Desktop/Images/images"];
-                            let _ = run_command(&run_ctxt);
-                        }
-                        _ => {}
+                    if let Some(cmd_ctxt) = cmd_config_map.get(&(key_code as u32)) {
+                        let _ = run_command(&cmd_ctxt);
                     }
                 }
                 _ => {}
             }
         }
     });
+    hotkey_handler
+}
+
+fn hotkey_register_and_monitor(
+    cmd_config_map: &'static HashMap<u32, Cmd_Context>,
+    terminal_rx: crossbeam_channel::Receiver<String>,
+    hotkey_tx: crossbeam_channel::Sender<i32>,
+) -> std::thread::JoinHandle<()> {
     let get_message_thread = spawn(move || unsafe {
-        let hotkey_ids = vec![VK_F1, VK_F2, VK_NUMPAD2];
-        let hotkey_ids_name = vec![stringify!(VK_F1), stringify!(VK_F2), stringify!(VK_NUMPAD2)];
-        for hotkey_idx in 0..hotkey_ids.len() {
-            println!(
-                "hotkey register: {} => {}",
-                hotkey_ids_name[hotkey_idx], hotkey_ids[hotkey_idx]
-            );
+        for (hotkey, cmd_context) in cmd_config_map {
             if RegisterHotKey(
                 null_mut(),
                 HOTKEY_ID,
                 (MOD_ALT | MOD_CONTROL | MOD_NOREPEAT) as UINT,
-                hotkey_ids[hotkey_idx] as UINT,
+                (*hotkey) as UINT,
             ) == 0
             {
                 eprintln!("register hot key error, exit {}", GetLastError());
                 return;
+            } else {
+                println!(
+                    "hotkey register: {} => {}",
+                    cmd_context.shortcut_key_name,
+                    cmd_context.shortcut_key_code.unwrap()
+                );
             }
         }
         let mut msg = MSG::default();
@@ -214,8 +300,5 @@ fn main() -> io::Result<()> {
         }
         let _ = UnregisterHotKey(null_mut(), HOTKEY_ID);
     });
-    /* waiting for the hook process thread */
-    while !get_message_thread.is_finished() {}
-    while !hotkey_handler.is_finished() {}
-    Ok(())
+    get_message_thread
 }
